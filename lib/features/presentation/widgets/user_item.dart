b1903a5d06c77b0ad7387f49cb9863b063a6072f
Miqdad <miqdaddeveloper@gmail.com>
2023-07-28 17:30:43 +0700

    (feat & fix) : Add _buildRemovableArea, onTap, onRemove

--- lib/features/presentation/widgets/user_item.dart
+++ lib/features/presentation/widgets/user_item.dart
@@ -5,52 +5,82 @@ import '../../../config/theme/app_theme.dart';
 
 class UserItem extends StatelessWidget {
   final UserEntity? user;
-  const UserItem({Key? key, this.user}) : super(key: key);
+  final void Function(UserEntity article)? onRemove;
+  final void Function(UserEntity article)? onTap;
+
+  const UserItem({Key? key, this.user, this.onRemove, this.onTap})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(
-        bottom: 18,
-      ),
-      padding: const EdgeInsets.all(22),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Colors.white,
-      ),
-      child: Row(
-        children: [
-          CircleAvatar(
-            radius: 22,
-            backgroundColor: Colors.grey.shade300,
-            child: const Icon(
-              Icons.person,
-              color: Colors.grey,
+    return GestureDetector(
+      onTap: _onTap,
+      child: Container(
+        margin: const EdgeInsets.only(
+          bottom: 18,
+        ),
+        padding: const EdgeInsets.all(22),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: Colors.white,
+        ),
+        child: Row(
+          children: [
+            CircleAvatar(
+              radius: 22,
+              backgroundColor: Colors.grey.shade300,
+              child: const Icon(
+                Icons.person,
+                color: Colors.grey,
+              ),
             ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                user!.name.toString(),
-                style: blackTextStyle.copyWith(
-                  fontSize: 16,
-                  fontWeight: medium,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  user!.name.toString(),
+                  style: blackTextStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: medium,
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 2,
-              ),
-              Text(
-                '@${user!.email.toString()}',
-                style: greyTextStyle.copyWith(
-                  fontSize: 12,
+                const SizedBox(
+                  height: 2,
                 ),
-              ),
-            ],
-          ),
-        ],
+                Text(
+                  user!.email.toString(),
+                  style: greyTextStyle.copyWith(
+                    fontSize: 12,
+                  ),
+                ),
+                _buildRemovableArea(),
+              ],
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  Widget _buildRemovableArea() {
+    return GestureDetector(
+      onTap: _onRemove,
+      child: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 8),
+        child: Icon(Icons.remove_circle_outline, color: Colors.red),
+      ),
+    );
+  }
+
+  void _onTap() {
+    if (onTap != null) {
+      onTap!(user!);
+    }
+  }
+
+  void _onRemove() {
+    if (onRemove != null) {
+      onRemove!(user!);
+    }
   }
 }
