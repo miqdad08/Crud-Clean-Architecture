d9b39b98636c4c795c85494202452848cd808dd5
Miqdad <miqdaddeveloper@gmail.com>
2023-07-28 16:41:21 +0700

    (feat & fix) : Add GetUsersUseCase

---
+++ lib/features/domain/usecases/get_users.dart
@@ -0,0 +1,18 @@
+import 'package:crud_clean_architecture/core/resources/failure.dart';
+import 'package:crud_clean_architecture/core/usecase/usecase.dart';
+import 'package:crud_clean_architecture/features/domain/entities/user.dart';
+import 'package:crud_clean_architecture/features/domain/repository/user_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class GetUsersUseCase implements UseCase<Either<Failure, List<UserEntity>>,void>{
+
+  final UserRepository _userRepository;
+
+  GetUsersUseCase(this._userRepository);
+
+  @override
+  Future<Either<Failure, List<UserEntity>>> call({void params}) {
+    return _userRepository.getUsers();
+  }
+
+}
