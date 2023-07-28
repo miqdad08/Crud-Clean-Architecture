a884bd4ed3d749cd922076e083f1cb1c78df3934
Miqdad <miqdaddeveloper@gmail.com>
2023-07-28 17:03:28 +0700

    (feat & fix) : Add UsersEvent, GetUsers, RemoveUser, AddUser, UpdateUser Event

--- lib/features/presentation/bloc/users/users_event.dart
+++ lib/features/presentation/bloc/users/users_event.dart
@@ -1,4 +1,28 @@
 part of 'users_bloc.dart';
 
 @immutable
-abstract class UsersEvent {}
+abstract class UsersEvent extends Equatable{
+  final UserEntity? user;
+
+  const UsersEvent({this.user});
+
+
+  @override
+  List<Object?> get props => [user];
+}
+
+class GetUsers extends UsersEvent {
+  const GetUsers();
+}
+
+class RemoveUser extends UsersEvent {
+  const RemoveUser(UserEntity user) : super(user: user);
+}
+
+class AddUser extends UsersEvent {
+  const AddUser(UserEntity user) : super(user: user);
+}
+
+class UpdateUser extends UsersEvent {
+  const UpdateUser(UserEntity user) : super(user: user);
+}
