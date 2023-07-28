b76510b099d368840624a9ee7d55d36f4d49fdec
Miqdad <miqdaddeveloper@gmail.com>
2023-07-28 17:20:41 +0700

    (feat & fix) : Add UserItem

---
+++ lib/features/presentation/widgets/user_item.dart
@@ -0,0 +1,56 @@
+import 'package:crud_clean_architecture/features/domain/entities/user.dart';
+import 'package:flutter/material.dart';
+
+import '../../../config/theme/app_theme.dart';
+
+class UserItem extends StatelessWidget {
+  final UserEntity? user;
+  const UserItem({Key? key, this.user}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(
+        bottom: 18,
+      ),
+      padding: const EdgeInsets.all(22),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.white,
+      ),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 22,
+            backgroundColor: Colors.grey.shade300,
+            child: const Icon(
+              Icons.person,
+              color: Colors.grey,
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                user!.name.toString(),
+                style: blackTextStyle.copyWith(
+                  fontSize: 16,
+                  fontWeight: medium,
+                ),
+              ),
+              const SizedBox(
+                height: 2,
+              ),
+              Text(
+                '@${user!.name.toString()}',
+                style: greyTextStyle.copyWith(
+                  fontSize: 12,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
