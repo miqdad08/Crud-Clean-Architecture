d3e984c10f6c2b1777e84f895b0dd1f6b4506d65
Miqdad <miqdaddeveloper@gmail.com>
2023-07-28 15:20:56 +0700

    (feat & fix) : Fix User Service

--- lib/features/data/data_source/remote/users_service.dart
+++ lib/features/data/data_source/remote/users_service.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 
 import 'package:crud_clean_architecture/core/constant/constant.dart';
 import 'package:crud_clean_architecture/features/data/models/user.dart';
-import 'package:dartz/dartz.dart';
 import 'package:http/http.dart' as http;
 import '../../../../core/resources/failure.dart';
 
@@ -11,71 +10,59 @@ class UsersService {
 
   UsersService(this.httpClient);
 
-  Future<Either<Failure, List<UserModel>>> getUsers() async {
-    try {
-      final response = await httpClient.get(Uri.parse('$baseUrl/users'));
+  Future<List<UserModel>> getUsers() async {
+    final response = await httpClient.get(Uri.parse('$baseUrl/users'));
 
-      if (response.statusCode == 200) {
-        final dataUsers = jsonDecode(response.body)['data'];
-        return Right(List<UserModel>.from(
-            dataUsers.map((user) => UserModel.fromJson(user))));
-      } else {
-        return Left(Failure(message: jsonDecode(response.body)['message']));
-      }
-    } catch (e) {
-      rethrow;
-    }
-  }
-  Future<Either<Failure, String>> removeUser(int userId) async {
-    try {
-      final response =
-      await httpClient.delete(Uri.parse('$baseUrl/users/$userId'));
-
-      if (response.statusCode == 200) {
-        return const Right('Success Remove User');
-      } else {
-        return Left(Failure(message: jsonDecode(response.body)['message']));
-      }
-    } catch (e) {
-      rethrow;
+    if (response.statusCode == 200) {
+      final dataUsers = jsonDecode(response.body)['data'];
+      return List<UserModel>.from(
+          dataUsers.map((user) => UserModel.fromJson(user)));
+    } else {
+      throw Failure(
+          message: jsonDecode(response.body)['message'] ?? 'Failed Get User');
     }
   }
 
-  Future<Either<Failure, String>> addUser(UserModel user) async {
-    try {
-      final response = await httpClient.post(
-          Uri.parse(
-            '$baseUrl/users',
-          ),
-          body: user.toJson(),
-      );
-      if (response.statusCode == 200) {
-        return const Right('Success Add User');
-      } else {
-        return Left(Failure(message: jsonDecode(response.body)['message']));
-      }
-    } catch (e) {
-      rethrow;
+  Future<String> removeUser(int userId) async {
+    final response =
+        await httpClient.delete(Uri.parse('$baseUrl/users/$userId'));
+
+    if (response.statusCode == 200) {
+      return 'Success Remove User';
+    } else {
+      throw Failure(
+          message:
+              jsonDecode(response.body)['message'] ?? 'Failed Remove User');
     }
   }
 
-  Future<Either<Failure, String>> editUser(UserModel user) async {
-    try {
-      final response = await httpClient.post(
-        Uri.parse(
-          '$baseUrl/users/${user.id}',
-        ),
-        body: user.toJson(),
-      );
-      if (response.statusCode == 200) {
-        return const Right('Success Edit User');
-      } else {
-        return Left(Failure(message: jsonDecode(response.body)['message']));
-      }
-    } catch (e) {
-      rethrow;
+  Future<String> addUser(UserModel user) async {
+    final response = await httpClient.post(
+      Uri.parse(
+        '$baseUrl/users',
+      ),
+      body: user.toJson(),
+    );
+    if (response.statusCode == 200) {
+      return 'Success Add User';
+    } else {
+      throw Failure(
+          message: jsonDecode(response.body)['message'] ?? 'Failed Add User');
     }
   }
 
-
+  Future<String> editUser(UserModel user) async {
+    final response = await httpClient.post(
+      Uri.parse(
+        '$baseUrl/users/${user.id}',
+      ),
+      body: user.toJson(),
+    );
+    if (response.statusCode == 200) {
+      return 'Success Edit User';
+    } else {
+      throw Failure(
+          message: jsonDecode(response.body)['message'] ?? 'Failed Edit User');
+    }
+  }
 }
