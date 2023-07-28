afbf87fb7ce3bc4a7befe15651a75dbed9e8a9bb
Miqdad <miqdaddeveloper@gmail.com>
2023-07-28 16:53:26 +0700

    (feat & fix) : Add UserState, UsersInitial, UsersLoading, UsersSuccess, UsersFailed

--- lib/features/presentation/bloc/users/users_state.dart
+++ lib/features/presentation/bloc/users/users_state.dart
@@ -1,6 +1,26 @@
 part of 'users_bloc.dart';
 
 @immutable
-abstract class UsersState {}
+abstract class UsersState extends Equatable {
+  final List<UserEntity>? users;
+  final Failure? error;
+
+  const UsersState({this.users, this.error});
+
+  @override
+  List<Object?> get props => [users!, error!];
+}
 
 class UsersInitial extends UsersState {}
+
+class UsersLoading extends UsersState {
+  const UsersLoading();
+}
+
+class UsersSuccess extends UsersState {
+  const UsersSuccess(List<UserEntity> users) : super(users: users);
+}
+
+class UsersFailed extends UsersState {
+  const UsersFailed(Failure error) : super(error: error);
+}
