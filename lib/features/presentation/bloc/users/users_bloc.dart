d5205a39f20e859890c5bec8cc127b080bf16708
Miqdad <miqdaddeveloper@gmail.com>
2023-07-28 14:00:20 +0700

    (feat & fix) : Add UsersBloc

---
+++ lib/features/presentation/bloc/users/users_bloc.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'users_event.dart';
+part 'users_state.dart';
+
+class UsersBloc extends Bloc<UsersEvent, UsersState> {
+  UsersBloc() : super(UsersInitial()) {
+    on<UsersEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
